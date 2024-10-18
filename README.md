73d5a6c3d3fc7cb32cd4ef31f4c197da8473d343
yhattav <yonatanh@wix.com>
2024-10-18 19:21:21 +0300

    add license

--- README.md
+++ README.md
@@ -105,3 +105,11 @@ Then, from within your project folder:
 npm run build
 surge public my-project.surge.sh
 ```
+
+## License
+
+This project is licensed under the Apache License, Version 2.0. See the [LICENSE](LICENSE) file for details.
+
+## Notice
+
+For additional attribution notices, please see the [NOTICE](NOTICE) file.
--- src/components/CenterText.svelte
+++ src/components/CenterText.svelte
@@ -1,13 +1,27 @@
-<script>
+/* Copyright 2024 Yonatan Hattav
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License. */
+
+<script lang="ts">
     export let center = '';
-</script>
-
-<span class="speed-reader-center">{center}</span>
-
-<style>
+  </script>
+  
+  <span class="speed-reader-center">{center}</span>
+  
+  <style>
     .speed-reader-center {
-        display: inline-block;
-        color: rgb(var(--offsetColor));
-        font-weight: bold;
+      display: inline-block;
+      color: #ff4500;
+      font-weight: bold;
     }
-</style>
+  </style>
